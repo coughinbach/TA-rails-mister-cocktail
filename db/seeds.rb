bac69ef5a48c985d913f122c7c81b9341bd09b38
Lola Barbier <lolabarbier@hotmail.com>
2018-02-13 13:44:27 +0100

    seeded ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "open-uri"
+require "json"
+require "faker"
+
+
+filepath = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+serialized_ingredients = open(filepath).read
+
+ingredients = JSON.parse(serialized_ingredients)
+
+puts "Creating ingredients..."
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+end
+p "done"
+
+# p "Creating cocktails"
+# 15.times do
+#   cocktail = Cocktail.new(name: Faker::Hipster.words(2, false, true).join(" ").capitalize)
+#   cocktail.remote_photo_url = photos.sample
+#   cocktail.save
+# end
+# p "done-zo"
